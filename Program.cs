0c9e315f9d42c87daf8b52108df93f6fcc3b8824
Roland4444 <turbopollux@rambler.ru>
2021-03-31 14:13:22 +0400

    rename

--- Program.cs
+++ Program.cs
@@ -1,34 +1,25 @@
 ﻿using System;
 using Npgsql;
 /////http://zetcode.com/csharp/postgresql/
-
 namespace EFGetStarted
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var cs = "Host=localhost;Username=user2;Password=123;Database=testdb";
+            var cs = "Host=localhost;Username=user2;Password=123;Database=users";
             using var con = new NpgsqlConnection(cs);
             con.Open();
-
-            var sql2 = "SELECT * FROM employee";
-
+            var sql2 = "SELECT * FROM users";
             var sql = "SELECT version()";
-
             using var cmd = new NpgsqlCommand(sql2, con);
-
-            var version = cmd.ExecuteScalar().ToString();
+            using var versioncmd = new NpgsqlCommand(sql, con);
+            var version = versioncmd.ExecuteScalar().ToString();
             Console.WriteLine($"PostgreSQL version: {version}");
-           
-
             using NpgsqlDataReader rdr = cmd.ExecuteReader();
-
             while (rdr.Read())
-            {
-                Console.WriteLine("{0} {1} {2}", rdr.GetInt32(0), rdr.GetString(1),
-                        rdr.GetString(2));
-            }
+                Console.WriteLine("{0} {1} ", rdr.GetString(0), rdr.GetString(1));
+            
         }
     }
 }
